204e2e5d465ea10882e80c0b83c30ac21bcea786
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2019-09-11 13:45:54 +0200

    Initial commit - add part 3, worksheet 1

---
+++ Worksheet 1/render02562/configuration.h
@@ -0,0 +1,9 @@
+#ifndef CONFIGURATIOM_H
+#define CONFIGURATIOM_H
+
+#include <string>
+
+std::string const models_path = "/Users/mateuszsadowski/Documents/programowanie/DTU/Rendering Introduction/Worksheet 1/render02562/models/";
+
+//CONFIGURATIOM_H
+#endif
