79c0312aae9be1be040b2e2eefaea11e0d165bb1
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2019-09-11 14:21:24 +0200

    Add part 3, worksheet 1

--- Worksheet 1/render02562/raytrace/RayCaster.cpp
+++ Worksheet 1/render02562/raytrace/RayCaster.cpp
@@ -12,65 +12,63 @@
 using namespace std;
 using namespace optix;
 
-float3 RayCaster::compute_pixel(unsigned int x, unsigned int y) const
-{
-  // Use the scene and its camera
-  // to cast a ray that computes the color of the pixel at index (x, y).
-  //
-  // Input:  x, y        (pixel index)
-  //
-  // Return: Result of tracing a ray through the pixel at index (x, y).
-  //
-  // Relevant data fields that are available (see RayCaster.h and Scene.h)
-  // win_to_ip           (pixel size (width, height) in the image plane)
-  // lower_left          (lower left corner of the film in the image plane)
-  // scene               (scene with access to the closest_hit function)
-  // scene->get_camera() (camera in the scene)
-  //
-  // Hints: (a) Use the function get_shader(...) to get the shader of the
-  //            intersected material after the ray has been traced.
-  //        (b) Use get_background(...) if the ray does not hit anything.
+float3 RayCaster::compute_pixel(unsigned int x, unsigned int y) const {
+    // Use the scene and its camera
+    // to cast a ray that computes the color of the pixel at index (x, y).
+    //
+    // Input:  x, y        (pixel index)
+    //
+    // Return: Result of tracing a ray through the pixel at index (x, y).
+    //
+    // Relevant data fields that are available (see RayCaster.h and Scene.h)
+    // win_to_ip           (pixel size (width, height) in the image plane)
+    // lower_left          (lower left corner of the film in the image plane)
+    // scene               (scene with access to the closest_hit function)
+    // scene->get_camera() (camera in the scene)
+    //
+    // Hints: (a) Use the function get_shader(...) to get the shader of the
+    //            intersected material after the ray has been traced.
+    //        (b) Use get_background(...) if the ray does not hit anything.
 
-  float2 ip_coords = make_float2(x, y) * win_to_ip + lower_left;
-  Ray r = scene->get_camera()->get_ray(ip_coords);
-  float3 result = (r.direction + 1) / 2.0;
+    float2 ip_coords = make_float2(x, y) * win_to_ip + lower_left;
+    Ray r = scene->get_camera()->get_ray(ip_coords);
 
-  return result;
+    HitInfo hit;
+    if (scene->closest_hit(r, hit)) {
+        return scene->get_shader(hit)->shade(r, hit);
+    } else {
+        return get_background();
+    }
 }
 
-float3 RayCaster::get_background(const float3& dir) const
-{ 
-  if(!sphere_tex)
-    return background;
-  return make_float3(sphere_tex->sample_linear(dir));
+float3 RayCaster::get_background(const float3 &dir) const {
+    if (!sphere_tex)
+        return background;
+    return make_float3(sphere_tex->sample_linear(dir));
 }
 
-void RayCaster::increment_pixel_subdivs()
-{
-  ++subdivs;
-  compute_jitters();
-  cout << "Rays per pixel: " << subdivs*subdivs << endl;
-}
-
-void RayCaster::decrement_pixel_subdivs()
-{
-  if(subdivs > 1)
-  {
-    --subdivs;
+void RayCaster::increment_pixel_subdivs() {
+    ++subdivs;
     compute_jitters();
-  }
-  cout << "Rays per pixel: " << subdivs*subdivs << endl;
+    cout << "Rays per pixel: " << subdivs * subdivs << endl;
 }
 
-void RayCaster::compute_jitters()
-{
-  float aspect = width/static_cast<float>(height);
-  win_to_ip.x = win_to_ip.y = 1.0f/static_cast<float>(height);
-  lower_left = (win_to_ip - make_float2(aspect, 1.0f))*0.5f;
-  step = win_to_ip/static_cast<float>(subdivs);
+void RayCaster::decrement_pixel_subdivs() {
+    if (subdivs > 1) {
+        --subdivs;
+        compute_jitters();
+    }
+    cout << "Rays per pixel: " << subdivs * subdivs << endl;
+}
 
-  jitter.resize(subdivs*subdivs);
-  for(unsigned int i = 0; i < subdivs; ++i)
-    for(unsigned int j = 0; j < subdivs; ++j)
-      jitter[i*subdivs + j] = make_float2(safe_mt_random() + j, safe_mt_random() + i)*step - win_to_ip*0.5f; 
+void RayCaster::compute_jitters() {
+    float aspect = width / static_cast<float>(height);
+    win_to_ip.x = win_to_ip.y = 1.0f / static_cast<float>(height);
+    lower_left = (win_to_ip - make_float2(aspect, 1.0f)) * 0.5f;
+    step = win_to_ip / static_cast<float>(subdivs);
+
+    jitter.resize(subdivs * subdivs);
+    for (unsigned int i = 0; i < subdivs; ++i)
+        for (unsigned int j = 0; j < subdivs; ++j)
+            jitter[i * subdivs + j] = make_float2(safe_mt_random() + j, safe_mt_random() + i) * step - win_to_ip * 0.5f;
 }
